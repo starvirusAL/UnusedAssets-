038df861d4200f2969a0878811f70f79776cc1bb
starvirusAL <alexoid1990@gmail.com>
2022-07-08 09:23:17 +0300

    V.1.1.1

---
+++ Assets/UnusedAssets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 0.12f;
+    [SerializeField] float xClamp;
+    [SerializeField] GameObject trail;
+
+    bool isActive = true;
+
+    public TouchPad touchPad;
+
+    void Update()
+    {
+        MovePlayerTower(isActive);
+
+    }
+
+    void MovePlayerTower(bool isActive)
+    {
+        
+        
+            Vector2 direction = touchPad.GetDirection();
+            float xOffset = direction.x * Time.deltaTime * speed;
+            float newXpos = transform.localPosition.x + xOffset;
+            float xMove = Mathf.Clamp(newXpos, -xClamp, xClamp);
+
+
+            transform.localPosition = new Vector3(xMove, transform.localPosition.y, transform.localPosition.z);
+        trail.transform.localPosition = new Vector3(xMove, 0.52f, transform.localPosition.z);
+
+    }
+
+
+}
